4fa3b423d3d9126b9f84ae5a204c88a7c23d7b4b
takeshi <panda531531@gmail.com>
2021-02-12 09:27:04 +0900

    current invalid app

--- src/extensions/scratch3_minecraft/index.js
+++ src/extensions/scratch3_minecraft/index.js
@@ -1,7 +1,16 @@
+// import { Rcon } from 'rcon-client'
+
 const ArgumentType = require('../../extension-support/argument-type');
 const BlockType = require('../../extension-support/block-type');
 const Cast = require('../../util/cast');
 const log = require('../../util/log');
+// const Rcon = require('rcon-client');
+// const Rcon = require('srcds-rcon');
+// const nets = require('nets');
+// const io = require('socket.io-client');
+// const Rcon = require('modern-rcon');
+const Rcon = require('rcon-srcds');
+// import Rcon from 'rcon-srcds';
 
 class Scratch3Minecraft {
 
@@ -34,7 +43,85 @@ class Scratch3Minecraft {
     writeLog(args) {
         const text = Cast.toString(args.TEXT);
         log.log(text);
+
+        const server = new Rcon({ host: '127.0.0.1', port: 25010 });
+
+        server.authenticate('')
+            .then(() => {
+                log.log('authenticated');
+                return server.execute('status');
+            })
+            .then(log.log)
+            .catch(console.error);
+
+
+
+        // const rcon = new Rcon({ host: "localhost", port: 25575, passward: '' });
+
+        // rcon.connect().then(() => {
+        //     return rcon.send('say hahaha'); // That's a command for Minecraft
+        // }).then(res => {
+        //     console.log(res);
+        // }).then(() => {
+        //     return rcon.disconnect();
+        // });
+
+
+        // const url = 'localhost';
+        // const options = {
+        //     'force new connection': true,
+        //     port: 25575
+        // };
+
+        // const socket = io.connect(url, options);
+        // // socket.on('connect', () => {
+        // //     log.log('connected!!');
+        // // });
+        // // socket.on('sendMsgFromServer', function (msg) { console.log("message:", msg); });
+
+        // // サーバーからのデータ受け取り処理
+        // socket.on('connect', () => { log.log('connected!!'); }); // 接続
+        // socket.on('disconnect', () => { log.log('disconnected!!'); }); // 切断
+        // // サーバーからクライアントへの送り返し
+        // socket.on('s2c_message', (data) => { log.log(data.value) });
+        // // クライアントからサーバーへ送信
+        // socket.emit('c2s_broadcast', { value: 'say bbb' });
+
+        // let rcon = Rcon({
+        //     address: 'localhost',
+        //     port: 25575
+        // });
+        // rcon.connect().then(() => {
+        //     log.log('connected');
+        // }).catch(console.error);
     }
+
+    // https://developer.valvesoftware.com/wiki/Source_RCON_Protocol
+    createRequest(type, id, body) {
+
+        var size = Buffer.byteLength(body) + 14,
+            buffer = new Buffer(size);
+
+        buffer.writeInt32LE(size - 4, 0);
+        buffer.writeInt32LE(id, 4);
+        buffer.writeInt32LE(type, 8);
+        buffer.write(body, 12, size - 2, "ascii");
+        buffer.writeInt16LE(0, size - 2);
+
+        return buffer;
+    };
+
+    readResponse(buffer) {
+
+        const response = {
+            size: buffer.readInt32LE(0),
+            id: buffer.readInt32LE(4),
+            type: buffer.readInt32LE(8),
+            body: buffer.toString("ascii", 12, data.length - 2)
+        }
+
+        return response;
+    };
 }
 
 module.exports = Scratch3Minecraft;
