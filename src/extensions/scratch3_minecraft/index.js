9a0c47ee834e61446d3e1593f3dc59639c806777
takeshi <panda531531@gmail.com>
2021-02-16 02:25:48 +0900

    add getCurrentPlayerPosition block and display X,Y,Z coordinates blocks

--- src/extensions/scratch3_minecraft/index.js
+++ src/extensions/scratch3_minecraft/index.js
@@ -10,7 +10,8 @@ class Scratch3Minecraft {
     constructor(runtime) {
         this.runtime = runtime;
 
-        this.webSocket = null;
+        this.ws1 = null;
+        this.ws2 = null;
         this.host = 'localhost';
     }
 
@@ -40,25 +41,65 @@ class Scratch3Minecraft {
                             defaultValue: 'hello'
                         }
                     }
+                },
+                {
+                    opcode: 'getPlayerPosition',
+                    blockType: BlockType.COMMAND,
+                    text: '現在位置を調べる'
+                },
+                {
+                    opcode: 'getPosX',
+                    text: 'X',
+                    blockType: BlockType.REPORTER
+                },
+                {
+                    opcode: 'getPosY',
+                    text: 'Y',
+                    blockType: BlockType.REPORTER
+                },
+                {
+                    opcode: 'getPosZ',
+                    text: 'Z',
+                    blockType: BlockType.REPORTER
                 }
+
+
             ],
             menus: {
             }
         };
     }
-    // e.currentTarget.send("world.setBlocks(1,1,1,2,2,2,98)");
-    // e.currentTarget.send("world.getPlayerId()");
-    // e.currentTarget.send("getPos(1)");
+
+    getPosX() {
+        const stage = this.runtime.getTargetForStage();
+        if (stage) {
+            return stage.posX;
+        }
+        return 0;
+    }
+    getPosY() {
+        const stage = this.runtime.getTargetForStage();
+        if (stage) {
+            return stage.posY;
+        }
+        return 0;
+    }
+    getPosZ() {
+        const stage = this.runtime.getTargetForStage();
+        if (stage) {
+            return stage.posZ;
+        }
+        return 0;
+    }
 
     setHost(args) {
         this.host = args.TEXT;
     }
 
     sendCommand(commands) {
-        log.log(this.webSocket);
-        if (this.webSocket === null || this.webSocket.readyState === 3) {
-            this.webSocket = new WebSocket("ws://" + this.host + ":14711");
-            this.webSocket.onopen = function (e) {
+        if (this.ws1 === null || this.ws1.readyState === 3) {
+            this.ws1 = new WebSocket("ws://" + this.host + ":14711");
+            this.ws1.onopen = function (e) {
                 log.log('onopen !!');
                 log.log(e);
                 commands.forEach(command => {
@@ -66,17 +107,17 @@ class Scratch3Minecraft {
                 });
                 e.currentTarget.close();
             }
-            this.webSocket.onmessage = function (e) {
+            this.ws1.onmessage = function (e) {
                 log.log('onmessage !!');
                 log.log(e);
                 e.currentTarget.close();
             }
-            this.webSocket.onclose = function (e) {
+            this.ws1.onclose = function (e) {
                 log.log('onclose !!');
                 log.log(e);
                 // e.currentTarget = null;
             };
-            this.webSocket.onerror = function (e) {
+            this.ws1.onerror = function (e) {
                 log.log('onerror !!');
                 log.log(e);
             };
@@ -87,6 +128,60 @@ class Scratch3Minecraft {
         const command = ["chat.post(" + args.TEXT + ")"];
         this.sendCommand(command);
     }
+
+    getPlayerPosition() {
+        if (this.ws1 === null || this.ws1.readyState === 3) {
+            this.ws1 = new WebSocket("ws://" + this.host + ":14711");
+            this.ws1.onopen = function (e) {
+                log.log('onopen !!');
+                log.log(e);
+                e.currentTarget.send("world.getPlayerIds()");
+            }
+            this.ws1.onmessage = this.createGetPosWs.bind(this);
+            this.ws1.onclose = function (e) {
+                log.log('onclose !!');
+                log.log(e);
+                // e.currentTarget = null;
+            };
+            this.ws1.onerror = function (e) {
+                log.log('onerror !!');
+                log.log(e);
+            };
+        }
+    }
+
+
+
+    createGetPosWs(event) {
+        log.log('getposws')
+        const playerID = event.data.replace(/\r?\n/g, "");
+        if (this.ws2 === null || this.ws2.readyState === 3) {
+            this.ws2 = new WebSocket("ws://" + this.host + ":14711");
+            this.ws2.onopen = function (e) {
+                log.log('onopen !!');
+                log.log(e);
+                e.currentTarget.send("entity.getPos(" + playerID + ")");
+            }
+            this.ws2.onmessage = function (e) {
+                log.log('onmessage on ws2');
+                log.log(e);
+                let posX = e.data.split(',')[0];
+                let posY = e.data.split(',')[1];
+                let posZ = e.data.split(',')[2];
+                const stage = this.runtime.getTargetForStage();
+                stage.posX = Cast.toNumber(posX);
+                stage.posY = Cast.toNumber(posY);
+                stage.posZ = Cast.toNumber(posZ);
+                e.currentTarget.close();
+            }.bind(this);
+            this.ws2.onclose = function (e) {
+                log.log('onclose ws2');
+                log.log(e);
+                this.ws1.close();
+            }.bind(this);
+        }
+    }
+
 }
 
 module.exports = Scratch3Minecraft;
