9966ed8c064fb219370e2e10dd28ac8553833a5f
takeshi <panda531531@gmail.com>
2021-02-19 14:49:56 +0900

    change extension name

--- src/extension-support/extension-manager.js
+++ src/extension-support/extension-manager.js
@@ -24,7 +24,7 @@ const builtinExtensions = {
     makeymakey: () => require('../extensions/scratch3_makeymakey'),
     boost: () => require('../extensions/scratch3_boost'),
     gdxfor: () => require('../extensions/scratch3_gdx_for'),
-    minecraft: () => require('../extensions/scratch3_minecraft')
+    Minecraft: () => require('../extensions/scratch3_minecraft')
 };
 
 /**
