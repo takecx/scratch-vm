ec9a79008d3a2ff0390a8b46e362b67c50e7b10f
takeshi <panda531531@gmail.com>
2021-04-07 23:47:26 +0900

    introduce utils class

---
+++ src/extensions/minecraft_utils/utils.js
@@ -0,0 +1,131 @@
+const log = require('../../util/log');
+const Cast = require('../../util/cast');
+
+class MinecraftUtils {
+
+    //  port = "53199";
+    port = "14711";
+    commandIntervalMsec = 200;
+    absoluteStr = 'absolute';
+    relativeStr = 'relative';
+    ws = null;
+    host = 'localhost';
+
+    constructor(runtime) {
+        this.runtime = runtime;
+    }
+
+    _createWebSocket() {
+        this.ws = new WebSocket("ws://" + this.host + ":" + this.port);
+    }
+
+    getWebSocket() {
+        if (this.ws === null) {
+            this._createWebSocket();
+        }
+        return this.ws;
+    }
+
+    setLatestExecuteTIme() {
+        this.latestExecuteTime = Date.now();
+    }
+
+    async _checkState() {
+        await this._checkWebsocketState.bind(this);
+        await this._checkExecTime();
+    }
+
+    async _checkWebsocketState() {
+        return new Promise(((resolve, reject) => {
+            if (this.ws.readyState === WebSocket.CLOSED || this.ws.readyState === WebSocket.CLOSING) {
+                this.ws = this._createWebSocket(this.host);
+                this.ws.onopen = function (e) {
+                    resolve();
+                }
+                this.ws.onerror = function (e) {
+                    reject();
+                }
+            } else {
+                resolve();
+            }
+        }));
+    }
+
+    async _checkExecTime() {
+        const elapsedTime = Date.now() - this.latestExecuteTime;
+        if (elapsedTime < this.commandIntervalMsec) {
+            await this._sleep(this.commandIntervalMsec - elapsedTime);
+        }
+    }
+
+    async _sleep(msec) {
+        return new Promise(function (resolve) {
+            setTimeout(function () { resolve() }, msec);
+        })
+    }
+
+    /**
+     * Create data for a menu in scratch-blocks format, consisting of an array of objects with text and
+     * value properties. The text is a translated string, and the value is one-indexed.
+     * @param  {object[]} info - An array of info objects each having a name property.
+     * @return {array} - An array of objects with text and value properties.
+     * @private
+     */
+    _buildMenu(info) {
+        return info.map((entry, index) => {
+            const obj = {};
+            obj.text = entry.name;
+            obj.value = String(index);
+            return obj;
+        });
+    }
+
+    _searchCoordinateMode(args) {
+        if (typeof args.STARTX === 'string' && args.STARTX.indexOf('~') !== -1) return this.relativeStr;
+        if (typeof args.STARTY === 'string' && args.STARTY.indexOf('~') !== -1) return this.relativeStr;
+        if (typeof args.STARTZ === 'string' && args.STARTZ.indexOf('~') !== -1) return this.relativeStr;
+        if (typeof args.ENDX === 'string' && args.ENDX.indexOf('~') !== -1) return this.relativeStr;
+        if (typeof args.ENDY === 'string' && args.ENDY.indexOf('~') !== -1) return this.relativeStr;
+        if (typeof args.ENDZ === 'string' && args.ENDZ.indexOf('~') !== -1) return this.relativeStr;
+
+        return this.absoluteStr;
+    }
+
+    async _sendCommand(command) {
+        await this._checkState(this.host, this.ws, this.latestExecuteTime);
+        return new Promise(((resolve, reject) => {
+            this.ws.send(command);
+            this.latestExecuteTime = Date.now();
+            resolve();
+            this.ws.onerror = function (e) {
+                this.latestExecuteTime = Date.now();
+                reject();
+            };
+        }).bind(this));
+    }
+
+    _convertStartPosToRelative(args) {
+        let startRelCoord = new Object();
+        const stage = this.runtime.getTargetForStage();
+        startRelCoord.X = typeof args.STARTX === 'string' ? Cast.toNumber(stage.posX) + Cast.toNumber(args.STARTX.split('~')[1]) : Cast.toNumber(args.STARTX);
+        startRelCoord.Y = typeof args.STARTY === 'string' ? Cast.toNumber(stage.posY) + Cast.toNumber(args.STARTY.split('~')[1]) : Cast.toNumber(args.STARTY);
+        startRelCoord.Z = typeof args.STARTZ === 'string' ? Cast.toNumber(stage.posZ) + Cast.toNumber(args.STARTZ.split('~')[1]) : Cast.toNumber(args.STARTZ);
+        return startRelCoord;
+    }
+
+    _convertEndPosToRelative(args) {
+        let endRelCoord = new Object();
+        const stage = this.runtime.getTargetForStage();
+        endRelCoord.X = typeof args.ENDX === 'string' ? Cast.toNumber(stage.posX) + Cast.toNumber(args.ENDX.split('~')[1]) : Cast.toNumber(args.ENDX);
+        endRelCoord.Y = typeof args.ENDY === 'string' ? Cast.toNumber(stage.posY) + Cast.toNumber(args.ENDY.split('~')[1]) : Cast.toNumber(args.ENDY);
+        endRelCoord.Z = typeof args.ENDZ === 'string' ? Cast.toNumber(stage.posZ) + Cast.toNumber(args.ENDZ.split('~')[1]) : Cast.toNumber(args.ENDZ);
+        return endRelCoord;
+    }
+
+    setHost(args) {
+        this.host = args.TEXT;
+    }
+
+}
+
+module.exports = MinecraftUtils;
