cb493eebe010d4f499f1278cad2ebb565345517f
takeshi <panda531531@gmail.com>
2021-04-07 23:47:14 +0900

    add new package

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,8 @@ const base = {
                 plugins: [
                     '@babel/plugin-syntax-dynamic-import',
                     '@babel/plugin-transform-async-to-generator',
-                    '@babel/plugin-proposal-object-rest-spread'],
+                    '@babel/plugin-proposal-object-rest-spread',
+                    '@babel/plugin-proposal-class-properties'],
                 presets: [['@babel/preset-env', { targets: { browsers: ['last 3 versions', 'Safari >= 8', 'iOS >= 8'] } }]]
             }
             // ,
