69b14bca39fdd206888180c5fb3fc29b09272a18
takeshi <panda531531@gmail.com>
2021-04-17 11:37:25 +0900

    move create websocket function position

--- src/extensions/minecraft_utils/utils.js
+++ src/extensions/minecraft_utils/utils.js
@@ -32,8 +32,6 @@ class MinecraftUtils {
         return new Promise(((resolve, reject) => {
             if (ws.readyState === WebSocket.CLOSED || ws.readyState === WebSocket.CLOSING) {
                 log.log("closed! reopen!");
-                ws.close();
-                ws = this._createWebSocket();
                 ws.onopen = function (e) {
                     resolve(ws);
                 }
@@ -41,6 +39,7 @@ class MinecraftUtils {
                     log.log('error!');
                     reject();
                 }
+                ws = this._createWebSocket();
             } else {
                 resolve(ws);
             }
