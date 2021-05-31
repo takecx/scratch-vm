513ffc11794f488df365ed82ba4dc62f155e46f5
takeshi <panda531531@gmail.com>
2021-06-01 00:19:25 +0900

    add particle info for 1.16.5 only

--- src/extensions/scratch3_minecraft/particle_info.js
+++ src/extensions/scratch3_minecraft/particle_info.js
@@ -1,42 +1,5 @@
 const formatMessage = require('format-message');
 
-// 1.12.2に無いぽいもの
-// 玄武岩の三角州の灰	minecraft:ash
-// ソウルサンドの泡	minecraft:bubble_column_up
-// ソウルサンドの泡の最上部	minecraft:bubble_pop
-// 焚き火の煙	minecraft:campfire_cosy_smoke
-// 干草の俵の上においた焚き火の煙	minecraft:campfire_signal_smoke
-// コンポスターのきらめき	minecraft:composter
-// 真紅の森の胞子	minecraft:crimson_spore
-// マグマブロックの泡	minecraft:current_down
-// イルカが泳ぐときのパーティクル	minecraft:dolphin
-// したたるハチミツ	minecraft:dripping_honey
-// したたる泣く黒曜石	minecraft:dripping_obsidian_tear
-// スプラッシュポーションなどの渦巻	minecraft:effect
-// エルダーガーディアンの呪い	minecraft:elder_guardian
-// エンチャントされた武器で攻撃したときのパーティクル	minecraft:enchanted_hit
-// ステータス効果の渦巻	minecraft:entity_effect
-// エンダードラゴン撃破	minecraft:explosion_emitter
-// ハチミツのしずく	minecraft:falling_honey
-// 溶岩のしずく	minecraft:falling_lava
-// ミツバチから落ちる蜜	minecraft:falling_nectar
-// 泣く黒曜石のしずく	minecraft:falling_obsidian_tear
-// 水のしずく	minecraft:falling_water
-// 花火の閃光	minecraft:flash
-// 食べたときなどのパーティクル	minecraft:item
-// 滴り落ちたハチミツ	minecraft:landing_honey
-// 滴り落ちた溶岩	minecraft:landing_lava
-// 滴り落ちた泣く黒曜石	minecraft:landing_obsidian_tear
-// コンジットのパーティクル	minecraft:nautilus
-// 消えるときの煙	minecraft:poof
-// 雨	minecraft:rain
-// くしゃみ	minecraft:sneeze
-// ソウルスピードのパーティクル	minecraft:soul
-// ソウルファイヤの炎	minecraft:soul_fire_flame
-// イカスミ	minecraft:squid_ink
-// 水中	minecraft:underwater
-// 歪んだ森の胞子	minecraft:warped_spore
-
 exports.genParticleInfo = () => {
     return [
         {
@@ -430,6 +393,304 @@ exports.genParticleInfo = () => {
             }),
             particleName: 'witchMagic',
             particleName1165: 'minecraft:witch'
+        },
+
+        // for 1.16.5 (doe's not exist 1.12.2)
+        {
+            name: formatMessage({
+                id: 'minecraft.particle.ash',
+                default: '玄武岩の三角州の灰'
+            }),
+            particleName: '',
+            particleName1165: 'minecraft:ash'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.particle.bubble_column_up',
+                default: 'ソウルサンドの泡'
+            }),
+            particleName: '',
+            particleName1165: 'minecraft:bubble_column_up'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.particle.bubble_pop',
+                default: 'ソウルサンドの泡の最上部'
+            }),
+            particleName: '',
+            particleName1165: 'minecraft:bubble_pop'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.particle.campfire_cosy_smoke',
+                default: '焚き火の煙'
+            }),
+            particleName: '',
+            particleName1165: 'minecraft:campfire_cosy_smoke'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.particle.campfire_signal_smoke',
+                default: '干草の俵の上においた焚き火の煙'
+            }),
+            particleName: '',
+            particleName1165: 'minecraft:campfire_signal_smoke'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.particle.composter',
+                default: 'コンポスターのきらめき'
+            }),
+            particleName: '',
+            particleName1165: 'minecraft:composter'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.particle.crimson_spore',
+                default: '真紅の森の胞子'
+            }),
+            particleName: '',
+            particleName1165: 'minecraft:crimson_spore'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.particle.current_down',
+                default: 'マグマブロックの泡'
+            }),
+            particleName: '',
+            particleName1165: 'minecraft:current_down'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.particle.dolphin',
+                default: ''
+            }),
+            particleName: 'イルカが泳ぐときのパーティクル',
+            particleName1165: 'minecraft:dolphin'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.particle.dripping_honey',
+                default: 'したたるハチミツ'
+            }),
+            particleName: '',
+            particleName1165: 'minecraft:dripping_honey'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.particle.dripping_obsidian_tear',
+                default: 'したたる泣く黒曜石'
+            }),
+            particleName: '',
+            particleName1165: 'minecraft:dripping_obsidian_tear'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.particle.effect',
+                default: 'スプラッシュポーションなどの渦巻'
+            }),
+            particleName: '',
+            particleName1165: 'minecraft:effect'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.particle.elder_guardian',
+                default: 'エルダーガーディアンの呪い'
+            }),
+            particleName: '',
+            particleName1165: 'minecraft:elder_guardian'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.particle.enchanted_hit',
+                default: 'エンチャントされた武器で攻撃したときのパーティクル'
+            }),
+            particleName: '',
+            particleName1165: 'minecraft:enchanted_hit'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.particle.entity_effect',
+                default: 'ステータス効果の渦巻'
+            }),
+            particleName: '',
+            particleName1165: 'minecraft:entity_effect'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.particle.explosion_emitter',
+                default: 'エンダードラゴン撃破'
+            }),
+            particleName: '',
+            particleName1165: 'minecraft:explosion_emitter'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.particle.falling_honey',
+                default: 'ハチミツのしずく'
+            }),
+            particleName: '',
+            particleName1165: 'minecraft:falling_honey'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.particle.falling_lava',
+                default: '溶岩のしずく'
+            }),
+            particleName: '',
+            particleName1165: 'minecraft:falling_lava'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.particle.falling_nectar',
+                default: 'ミツバチから落ちる蜜'
+            }),
+            particleName: '',
+            particleName1165: 'minecraft:falling_nectar'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.particle.falling_obsidian_tear',
+                default: '泣く黒曜石のしずく'
+            }),
+            particleName: '',
+            particleName1165: 'minecraft:falling_obsidian_tear'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.particle.falling_water',
+                default: '水のしずく'
+            }),
+            particleName: '',
+            particleName1165: 'minecraft:falling_water'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.particle.flash',
+                default: '花火の閃光'
+            }),
+            particleName: '',
+            particleName1165: 'minecraft:flash'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.particle.item',
+                default: '食べたときなどのパーティクル'
+            }),
+            particleName: '',
+            particleName1165: 'minecraft:item'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.particle.landing_honey',
+                default: '滴り落ちたハチミツ'
+            }),
+            particleName: '',
+            particleName1165: 'minecraft:landing_honey'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.particle.landing_lava',
+                default: '滴り落ちた溶岩'
+            }),
+            particleName: '',
+            particleName1165: 'minecraft:landing_lava'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.particle.landing_obsidian_tear',
+                default: '滴り落ちた泣く黒曜石'
+            }),
+            particleName: '',
+            particleName1165: 'minecraft:landing_obsidian_tear'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.particle.nautilus',
+                default: 'コンジットのパーティクル'
+            }),
+            particleName: '',
+            particleName1165: 'minecraft:nautilus'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.particle.poof',
+                default: '消えるときの煙'
+            }),
+            particleName: '',
+            particleName1165: 'minecraft:poof'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.particle.rain',
+                default: '雨'
+            }),
+            particleName: '',
+            particleName1165: 'minecraft:rain'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.particle.sneeze',
+                default: 'くしゃみ'
+            }),
+            particleName: '',
+            particleName1165: 'minecraft:sneeze'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.particle.soul',
+                default: 'ソウルスピードのパーティクル'
+            }),
+            particleName: '',
+            particleName1165: 'minecraft:soul'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.particle.soul_fire_flame',
+                default: 'ソウルファイヤの炎'
+            }),
+            particleName: '',
+            particleName1165: 'minecraft:soul_fire_flame'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.particle.squid_ink',
+                default: 'イカスミ'
+            }),
+            particleName: '',
+            particleName1165: 'minecraft:squid_ink'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.particle.underwater',
+                default: '水中'
+            }),
+            particleName: '',
+            particleName1165: 'minecraft:underwater'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.particle.warped_spore',
+                default: '歪んだ森の胞子'
+            }),
+            particleName: '',
+            particleName1165: 'minecraft:warped_spore'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.particle.reverse_portal',
+                default: '逆ポータル'
+            }),
+            particleName: '',
+            particleName1165: 'minecarft:reverse_portal'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.particle.white_ash',
+                default: '白い灰'
+            }),
+            particleName: '',
+            particleName1165: 'minecraft:white_ash'
         }
     ];
 };
