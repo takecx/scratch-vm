dbaef1ec0220ccf55f42b6831021f6e7620ca74f
takecx <panda531531@gmai.com>
2025-05-05 22:09:20 +0900

    1.20.1のエンチャントに対応

--- src/extensions/scratch3_minecraft/enchant_info.js
+++ src/extensions/scratch3_minecraft/enchant_info.js
@@ -7,238 +7,287 @@ exports.getEnchantInfo = () => {
                 id: 'minecraft.protection',
                 default: 'ダメージ軽減'
             }),
-            id: 'protection'
+            id: 'minecraft:protection'
         },
         {
             name: formatMessage({
                 id: 'minecraft.fire_protection',
                 default: '火炎耐性'
             }),
-            id: 'fire_protection'
-        },
-        {
-            name: formatMessage({
-                id: 'minecraft.feather_falling',
-                default: '落下耐性'
-            }),
-            id: 'feather_falling'
+            id: 'minecraft:fire_protection'
         },
         {
             name: formatMessage({
                 id: 'minecraft.blast_protection',
                 default: '爆発耐性'
             }),
-            id: 'blast_protection'
+            id: 'minecraft:blast_protection'
         },
         {
             name: formatMessage({
                 id: 'minecraft.projectile_protection',
                 default: '飛び道具耐性'
             }),
-            id: 'projectile_protection'
+            id: 'minecraft:projectile_protection'
         },
         {
             name: formatMessage({
                 id: 'minecraft.thorns',
                 default: '棘の鎧'
             }),
-            id: 'thorns'
+            id: 'minecraft:thorns'
         },
         {
             name: formatMessage({
                 id: 'minecraft.respiration',
                 default: '水中呼吸'
             }),
-            id: 'respiration'
-        },
-        {
-            name: formatMessage({
-                id: 'minecraft.depth_strider',
-                default: '水中歩行'
-            }),
-            id: 'depth_strider'
+            id: 'minecraft:respiration'
         },
         {
             name: formatMessage({
                 id: 'minecraft.aqua_affinity',
                 default: '水中採掘'
             }),
-            id: 'aqua_affinity'
+            id: 'minecraft:aqua_affinity'
         },
         {
             name: formatMessage({
-                id: 'minecraft.sharpness',
-                default: 'ダメージ増加'
+                id: 'minecraft.depth_strider',
+                default: '水中歩行'
             }),
-            id: 'sharpness'
+            id: 'minecraft:depth_strider'
         },
         {
             name: formatMessage({
-                id: 'minecraft.smite',
-                default: 'アンデッド特効'
+                id: 'minecraft.feather_falling',
+                default: '落下耐性'
             }),
-            id: 'smite'
-        },
-        {
-            name: formatMessage({
-                id: 'minecraft.bane_of_arthropods',
-                default: '虫特効'
-            }),
-            id: 'bane_of_arthropods'
-        },
-        {
-            name: formatMessage({
-                id: 'minecraft.knockback',
-                default: 'ノックバック'
-            }),
-            id: 'knockback'
-        },
-        {
-            name: formatMessage({
-                id: 'minecraft.fire_aspect',
-                default: '火属性'
-            }),
-            id: 'fire_aspect'
-        },
-        {
-            name: formatMessage({
-                id: 'minecraft.looting',
-                default: 'ドロップ増加'
-            }),
-            id: 'looting'
-        },
-        {
-            name: formatMessage({
-                id: 'minecraft.efficiency',
-                default: '効率強化'
-            }),
-            id: 'efficiency'
-        },
-        {
-            name: formatMessage({
-                id: 'minecraft.silk_touch',
-                default: 'シルクタッチ'
-            }),
-            id: 'silk_touch'
-        },
-        {
-            name: formatMessage({
-                id: 'minecraft.unbreaking',
-                default: '耐久力'
-            }),
-            id: 'unbreaking'
-        },
-        {
-            name: formatMessage({
-                id: 'minecraft.fortune',
-                default: '幸運'
-            }),
-            id: 'fortune'
-        },
-        {
-            name: formatMessage({
-                id: 'minecraft.power',
-                default: '射撃ダメージ増加'
-            }),
-            id: 'power'
-        },
-        {
-            name: formatMessage({
-                id: 'minecraft.punch',
-                default: 'パンチ'
-            }),
-            id: 'punch'
-        },
-        {
-            name: formatMessage({
-                id: 'minecraft.flame',
-                default: 'フレイム'
-            }),
-            id: 'flame'
-        },
-        {
-            name: formatMessage({
-                id: 'minecraft.infinity',
-                default: '無限'
-            }),
-            id: 'infinity'
-        },
-        {
-            name: formatMessage({
-                id: 'minecraft.luck_of_the_sea',
-                default: '宝釣り'
-            }),
-            id: 'luck_of_the_sea'
-        },
-        {
-            name: formatMessage({
-                id: 'minecraft.lure',
-                default: '入れ食い'
-            }),
-            id: 'lure'
-        },
-        {
-            name: formatMessage({
-                id: 'minecraft.mending',
-                default: '修繕'
-            }),
-            id: 'mending'
+            id: 'minecraft:feather_falling'
         },
         {
             name: formatMessage({
                 id: 'minecraft.frost_walker',
                 default: '氷渡り'
             }),
-            id: 'frost_walker'
+            id: 'minecraft:frost_walker'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.soul_speed',
+                default: 'ソウルスピード'
+            }),
+            id: 'minecraft:soul_speed'
         },
         {
             name: formatMessage({
                 id: 'minecraft.binding_curse',
                 default: '束縛の呪い'
             }),
-            id: 'binding_curse'
+            id: 'minecraft:binding_curse'
         },
         {
             name: formatMessage({
                 id: 'minecraft.vanishing_curse',
                 default: '消滅の呪い'
             }),
-            id: 'vanishing_curse'
+            id: 'minecraft:vanishing_curse'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.sharpness',
+                default: '鋭さ'
+            }),
+            id: 'minecraft:sharpness'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.smite',
+                default: '聖なる力'
+            }),
+            id: 'minecraft:smite'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.bane_of_arthropods',
+                default: '虫殺し'
+            }),
+            id: 'minecraft:bane_of_arthropods'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.knockback',
+                default: 'ノックバック'
+            }),
+            id: 'minecraft:knockback'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.fire_aspect',
+                default: '火属性'
+            }),
+            id: 'minecraft:fire_aspect'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.looting',
+                default: 'アイテムボーナス'
+            }),
+            id: 'minecraft:looting'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.sweeping_edge',
+                default: '範囲ダメージ増加'
+            }),
+            id: 'minecraft:sweeping_edge'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.efficiency',
+                default: '効率強化'
+            }),
+            id: 'minecraft:efficiency'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.silk_touch',
+                default: 'シルクタッチ'
+            }),
+            id: 'minecraft:silk_touch'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.fortune',
+                default: '幸運'
+            }),
+            id: 'minecraft:fortune'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.unbreaking',
+                default: '耐久力'
+            }),
+            id: 'minecraft:unbreaking'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.mending',
+                default: '修繕'
+            }),
+            id: 'minecraft:mending'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.power',
+                default: 'パワー'
+            }),
+            id: 'minecraft:power'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.punch',
+                default: '衝撃'
+            }),
+            id: 'minecraft:punch'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.flame',
+                default: '火炎'
+            }),
+            id: 'minecraft:flame'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.infinity',
+                default: '無限'
+            }),
+            id: 'minecraft:infinity'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.luck_of_the_sea',
+                default: '宝釣り'
+            }),
+            id: 'minecraft:luck_of_the_sea'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.lure',
+                default: '入れ食い'
+            }),
+            id: 'minecraft:lure'
         },
         {
             name: formatMessage({
                 id: 'minecraft.impaling',
-                default: '水生特効'
+                default: '串刺し'
             }),
-            id: 'impaling'
+            id: 'minecraft:impaling'
         },
         {
             name: formatMessage({
                 id: 'minecraft.riptide',
                 default: '激流'
             }),
-            id: 'riptide'
+            id: 'minecraft:riptide'
         },
         {
             name: formatMessage({
                 id: 'minecraft.loyalty',
                 default: '忠誠'
             }),
-            id: 'loyalty'
+            id: 'minecraft:loyalty'
         },
         {
             name: formatMessage({
                 id: 'minecraft.channeling',
                 default: '召雷'
             }),
-            id: 'channeling'
+            id: 'minecraft:channeling'
         },
         {
             name: formatMessage({
-                id: 'minecraft.sweeping_edge',
-                default: '範囲ダメージ増加‌'
+                id: 'minecraft.multishot',
+                default: '拡散'
             }),
-            id: 'sweeping_edge'
-        }
+            id: 'minecraft:multishot'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.piercing',
+                default: '貫通'
+            }),
+            id: 'minecraft:piercing'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.quick_charge',
+                default: '高速装填'
+            }),
+            id: 'minecraft:quick_charge'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.breach',
+                default: 'ブリーチ'
+            }),
+            id: 'minecraft:breach'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.density',
+                default: '密度'
+            }),
+            id: 'minecraft:density'
+        },
+        {
+            name: formatMessage({
+                id: 'minecraft.wind_burst',
+                default: 'ウィンドバースト'
+            }),
+            id: 'minecraft:wind_burst'
+        },
     ];
-};
+}
